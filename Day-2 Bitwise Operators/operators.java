e3004d61a45af98a773a55f68129a4fe4cf75787
Bala Likhith Kanigolla <55309319+Likhithlpu@users.noreply.github.com>
2021-10-28 23:13:30 +0530

    Create operators.java

---
+++ Day-2 Bitwise Operators/operators.java
@@ -0,0 +1,14 @@
+
+
+import java.util.*;
+class operators{
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt(), y=scn.nextInt();
+        System.out.println(x&y); //And Gate or And Operator
+        System.out.println(x|y); //Or Gate or OR operator
+        System.out.println(x^y); //XOR Gate or XOR operator
+        System.out.println(~x); //Bitwise Not of x
+        System.out.println(~y); //Bitwise Not of y
+    }
+}
