6b490a927ff5e39f14a94da0f050a6bdaf96159c
Bala Likhith Kanigolla <55309319+Likhithlpu@users.noreply.github.com>
2021-10-29 13:53:39 +0530

    Update countnoofbitsdriver.java

--- Day-2 Bitwise Operators/countnoofbitsdriver.java
+++ Day-2 Bitwise Operators/countnoofbitsdriver.java
@@ -4,9 +4,9 @@ class nofbits{
    static int countbits(int n){
     int count=0;
     while(n>0){
-        if(n%2!=0)
+        if((n&1)==1)
             count++;
-        n=n/2;
+       n=n>>1;
     }
     return count;
     }
