25a5145994f8af8a0911c2db79c4b7deb5c89871
Bala Likhith Kanigolla <55309319+Likhithlpu@users.noreply.github.com>
2021-11-17 23:04:54 +0530

    Update power_of_two_constant.java

--- Day-2 Bitwise Operators/power_of_two_constant.java
+++ Day-2 Bitwise Operators/power_of_two_constant.java
@@ -1,5 +1,16 @@
 // Power of two or not
 //Example 6 is false and 4 is true
+//explanation of the condition used
+//when you subract one from the number in the binary representation the zeros after the last set bit number becomes one and last set bit becomes zero
+//this is how its works
+//n  =40 - 101000 
+//n-1=39 - 100111 doing the and of these both two gives 
+//         ______
+//         100000 = 32 and count increases by 1
+//         011111 = 31(n-1:32-1=31)
+//         ______
+//         000000 = 0 Count increases by two and loop terminates
+// This is how the condition works
 
 import java.util.*;
 class poweroftwo{
