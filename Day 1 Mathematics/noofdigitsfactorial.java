41df2ba9b4e0ba9947a9bb9eb743df8ea1b0ea96
Bala Likhith Kanigolla <55309319+Likhithlpu@users.noreply.github.com>
2022-01-18 16:05:38 +0530

    Create noofdigitsfactorial.java

---
+++ Day 1 Mathematics/noofdigitsfactorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main{
+    
+    public static long factorial(long n) {
+        long fac=1,i;
+        for(i=n;i>1;i--){
+            fac=fac*i;
+        }
+        
+        String count=Long.toString(fac);
+        return(+count.length()) ;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        long n=scn.nextLong();
+        System.out.println(factorial(n));
+    }
+}
