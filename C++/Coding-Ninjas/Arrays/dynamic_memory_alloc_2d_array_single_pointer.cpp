d1d2e1bfecbeb9e73d3ec5f04feb7f634d53f8c7
Bala Likhith Kanigolla <55309319+Likhithlpu@users.noreply.github.com>
2023-07-21 00:33:36 +0530

    Added Large Allocation

--- C++/Coding-Ninjas/Arrays/dynamic_memory_alloc_2d_array_single_pointer.cpp
+++ C++/Coding-Ninjas/Arrays/dynamic_memory_alloc_2d_array_single_pointer.cpp
@@ -12,12 +12,8 @@ int main()
 
     //Dynamically create the array of pointers of size row 
 
-    int **Arr= new int *[row];
+    int *Arr= new int [row*col];
 
-    //Dynamically allocate memory of c0lumn for each row from 0 to row -1 
-
-    for (int i = 0; i<row ; i++)
-        Arr[i]=new int[col];
 
 //Now the pointers are create for all the elements in the array
 //Its time to add the elements/values to it. 
@@ -25,7 +21,7 @@ int main()
     for(int i=0;i<row;i++){
         for ( int j = 0 ; j<col;j++){
             //Assing the value as some random value
-            Arr[i][j]=rand()%100;
+            *(Arr+i*col+j)=rand()%100;
         }
     }
 
@@ -33,16 +29,14 @@ int main()
 
     for (int i = 0 ;i<row;i++){
         for(int j=0;j<col;j++){
-            cout<<Arr[i][j]<<" ";
+            cout<<*(Arr+i*col+j)<<" ";
         }
         cout<<endl;
     }
 
     //Since we allocated some memory to the pointers and we need to deallocate the memory for the same
 
-    for (int i = 0; i<row;i++){
-        delete[] Arr[i];
-    }
+
     delete[] Arr;
     return 0;
 }
