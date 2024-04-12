8329ee1328dada217c2bd9050c384accdc36b60a
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-04-12 23:57:08 +0530

    Prime numbers

--- C++/GFG/Mathematics/prime_number.cpp
+++ C++/GFG/Mathematics/prime_number.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 using namespace std; 
 
+// For prime numbers, checking divisibility up to n/2 is sufficient because no integer greater than n/2 can evenly divide n without exceeding n.
+
+
 bool prime(int n){
-    for (int i=2;i<=n/2;i++){
-        if(n%i==0)
+    // Check divisibility of 'n' by numbers up to half of 'n'
+    // If 'n' is divisible by any number between 2 and n/2, it's not a prime
+    for (int i = 2; i <= n / 2; i++) {
+        // Check if 'n' is divisible by 'i' without any remainder
+        if (n % i == 0) {
+            // If 'n' is divisible by 'i', it's not a prime number
             return false;
+        }
     }
+    // If 'n' is not divisible by any number between 2 and n/2,
+    // it doesn't have any factors other than 1 and itself, so it's prime
     return true;
 }
+
 
 int main(){
     int n; 
