38cf29836e1445edfe89addb9986dd15cc63e37f
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-04-12 23:34:33 +0530

    Added effective meathod

--- C++/GFG/Mathematics/digits_of_factorial.cpp
+++ C++/GFG/Mathematics/digits_of_factorial.cpp
@@ -24,6 +24,23 @@ int digits_of_a_factorial(int n){
     return count;
 }
 
+    // effective meathod
+    int digitsInFactorial(int N)
+    {
+        // code 
+         if(N<0)
+    return 0;
+    if(N<=1)
+    return 1;
+    else
+    {
+        double d=0;
+        for(int i=2;i<=N;i++)
+            d+=log10(i);
+        return floor(d)+1;
+    }
+    }
+
 
 int main(){
     int n; 
@@ -31,4 +48,5 @@ int main(){
     cin >> n;
     cout<<"Factorial of n is: "<< factorial(n)<<endl;
     cout<< "No of Digits in factorial of "<<n<<" is "<< digits_of_a_factorial(n)<<endl;
+    cout<<"Effective meathod: "<<digitsInFactorial(n)<<endl;
 }
