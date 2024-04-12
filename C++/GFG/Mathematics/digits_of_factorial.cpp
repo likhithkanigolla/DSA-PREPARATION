11c1da6d95413bab54eeec9f19d75ec4af1e88d2
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-04-12 23:35:47 +0530

    fixed alignment

--- C++/GFG/Mathematics/digits_of_factorial.cpp
+++ C++/GFG/Mathematics/digits_of_factorial.cpp
@@ -29,16 +29,16 @@ int digits_of_a_factorial(int n){
     {
         // code 
          if(N<0)
-    return 0;
-    if(N<=1)
-    return 1;
-    else
-    {
-        double d=0;
-        for(int i=2;i<=N;i++)
-            d+=log10(i);
-        return floor(d)+1;
-    }
+            return 0;
+        if(N<=1)
+            return 1;
+        else
+        {
+            double d=0;
+            for(int i=2;i<=N;i++)
+                d+=log10(i);
+            return floor(d)+1;
+        }
     }
 
 
