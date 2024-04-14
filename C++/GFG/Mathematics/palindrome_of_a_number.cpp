38e17d32448bb5d6fd25b249d786ee5271bff647
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-04-15 00:06:05 +0530

    Added Explanation

--- C++/GFG/Mathematics/palindrome_of_a_number.cpp
+++ C++/GFG/Mathematics/palindrome_of_a_number.cpp
@@ -3,6 +3,13 @@
 using namespace std; 
 
 
+// Example n = 2002
+// itration 1 -> remainder(2002%10)  = 2 -> reverse = 0*10 + 2 = 2, n = 2002/10 = 200
+// itration 2 -> remainder(200%10)  = 0 -> reverse = 2*10 + 0 = 20, n = 200/10 = 20
+// itration 3 -> remainder(20%10)  = 0 -> reverse = 20*10 + 0 = 200, n = 20/10 = 2
+// itration 4 -> remainder(2%10)  = 2 -> reverse = 200*10 + 2 = 2002, n = 2/10 = 0
+
+
 int reverse(int n){
     int reverse_number=0;
     while(n>0){
