43d2e6cd1a3794117595e3190cc877856aba6280
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-04-15 23:09:58 +0530

    power of 2

---
+++ C++/GFG/Bit Magic/power_of_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    // Check if 'n' is positive
+    if (n <= 0)
+        return false;
+    
+    // Check if 'n' is a power of 10
+    double exponent = log2(n);
+    return exponent == floor(exponent);
+}
+
+int main(){
+    int n; 
+    cout<<"Enter a Number: "; 
+    cin>>n; 
+    cout << isPowerOfTwo(n) << endl;
+}
