0f812f5dba86a2351b46301eb19d47bd370f4547
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-08-07 01:36:51 +0530

    Added Compiler Configiration

---
+++ C++/Striver-A2Z-Sheet/Step-0-Runner/demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// Add any other specific header files you need
+using namespace std;
+
+int main() {
+    // Write your code here
+    string name;
+    cin >> name;
+    cout << "Hello "<< name << endl;
+    return 0;
+}
