248468c14ee9bdbd4f9d0101ec972d5697dc80ef
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-08-12 01:42:39 +0530

    P15

---
+++ C++/Striver-A2Z-Sheet/Step 1 - Learn the Basics/Step 1.2 - Pattrens/Pattren 15.cpp
@@ -0,0 +1,33 @@
+// Problem statement
+// Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Reverse Letter Triangle.
+
+// You must print a matrix corresponding to the given Reverse Letter Triangle.
+
+// Example:
+// Input: ‘N’ = 3
+
+// Output: 
+
+// A B C
+// A B
+// A
+
+#include <iostream>
+using namespace std;
+
+void nLetterTriangle(int n) {
+    // Write your code here.
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<char(64+j)<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    nLetterTriangle(n);
+    return 0;
+}
