1887a72f09f446c16bd026f07a21c6244f1c8bcf
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-08-12 01:32:13 +0530

    Increasing Number Triangle P13

---
+++ C++/Striver-A2Z-Sheet/Step 1 - Learn the Basics/Step 1.2 - Pattrens/Pattren 13.cpp
@@ -0,0 +1,34 @@
+// Problem statement
+// Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Increasing Number Triangle.
+
+// Example:
+// Input: ‘N’ = 3
+
+// Output: 
+
+// 1
+// 2 3
+// 4 5 6
+
+#include <iostream>
+using namespace std;
+
+void nNumberTriangle(int n) {
+    // Write your code here.
+    int number=1;
+    for(int i=1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            cout<<number<<" ";
+            number+=1;
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    nNumberTriangle(n);
+    return 0;
+}
+
