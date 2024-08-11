884ad51452fcc4f80856ef50a90b2d25cbf06ae3
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-08-12 01:38:01 +0530

    Increasing Letter Triangle P14

---
+++ C++/Striver-A2Z-Sheet/Step 1 - Learn the Basics/Step 1.2 - Pattrens/Pattren 14.cpp
@@ -0,0 +1,48 @@
+// Problem statement
+// Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Increasing Letter Triangle.
+
+// Example:
+// Input: ‘N’ = 3
+
+// Output: 
+
+// A
+// A B
+// A B C
+
+
+#include <iostream>
+using namespace std;
+
+
+// Approach 1
+// void nLetterTriangle(int n) {
+//     // Write your code here.
+//     char letter='A';
+//     for(int i=1;i<=n;i++){
+//         for (int j=1;j<=i;j++){
+//             cout<<letter<<" ";
+//             letter+=1;
+//         }
+//         cout<<endl;
+//         letter='A';
+//     }
+// }
+
+void nLetterTriangle(int n) {
+    // Write your code here.
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<char(64+j)<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    nLetterTriangle(n);
+    return 0;
+}
+
