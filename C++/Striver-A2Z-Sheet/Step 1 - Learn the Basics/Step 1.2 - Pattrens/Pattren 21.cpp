d150130c9f0918d151f9d0b3044c3331aa9c2926
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-08-15 00:36:48 +0530

    chore: Add code for generating N-shaped star pattern in C++

---
+++ C++/Striver-A2Z-Sheet/Step 1 - Learn the Basics/Step 1.2 - Pattrens/Pattren 21.cpp
@@ -0,0 +1,46 @@
+// Problem statement
+// Ninja has been given a task to print the required star pattern and he asked your help for the same.
+
+// You must return an ‘N’*’N’ matrix corresponding to the given star pattern.
+
+// Example:
+// Input: ‘N’ = 4
+
+// Output: 
+
+// ****
+// *  *
+// *  *
+// ****
+
+
+#include <iostream>
+using namespace std;
+
+void getStarPattern(int n) {
+    // Write your code here.
+    for(int i=1;i<=n;i++){
+        if(i==1||i==n){
+            for(int j=1;j<=n;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        else{
+            cout<<"*";
+            for(int j=2;j<=n-1;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+            cout<<endl;
+        }
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    getStarPattern(n);
+    return 0;
+}
