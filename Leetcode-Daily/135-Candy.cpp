ac286a7300e81f7dc98cb0975224a92b5c998fdf
Likhith Kanigolla <likhithkanigolla@gmail.com>
2025-06-02 23:05:10 +0530

    Implement candy distribution algorithm in C++ and Python

---
+++ Leetcode-Daily/135-Candy.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int> candies(n,1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i]=1+candies[i-1];
+            }
+        }
+
+        int total= candies[n-1];
+        for(int i=n-1;i>0;--i){
+            if(ratings[i-1]>ratings[i]){
+                candies[i-1]=max(candies[i-1],candies[i]+1);
+            }
+            total += candies[i-1];
+        }
+        return total;
+    }
+};
