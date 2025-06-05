6cf1dc8ccf6f25588a5a896a2ab0bd15a0739d41
Likhith Kanigolla <likhithkanigolla@gmail.com>
2025-06-06 01:32:59 +0530

    Add alternative approach for printing Alpha-Triangle pattern

--- C++/Striver-A2Z-Sheet/Step 1 - Learn the Basics/Step 1.2 - Pattrens/Pattren 18.cpp
+++ C++/Striver-A2Z-Sheet/Step 1 - Learn the Basics/Step 1.2 - Pattrens/Pattren 18.cpp
@@ -26,6 +26,27 @@ void alphaTriangle(int n) {
     }
 }
 
+// Approach 2
+// class Solution {
+// public:
+//     //Function to print pattern18
+//     void pattern18(int n) {
+//         // Outer loop for the number of rows.
+//         for(int i = 0; i < n; i++) {
+            
+//             /* Inner loop for printing alphabets
+//             from A + n -1 -i (i is row no.) to
+//             A + n -1 ( E in this case).*/
+//             for(char ch = ('A'+ n-1)-i; ch <= ('A'+ n-1); ch++){
+//                 cout<<ch<<" ";
+//             }
+            
+//             // Move to the next line for the next row.
+//             cout << endl;
+//         }
+//     }
+// };
+
 int main() {
     int n;
     cin >> n;
